79f355b805e00b0c6da955c2e1e2bcc2b651c03f
yamakassi <40936571+yamakassi@users.noreply.github.com>
2022-11-11 22:18:14 +0400

    Add files via upload

--- JavaScriptObfuscator.js
+++ JavaScriptObfuscator.js
@@ -14,6 +14,8 @@ const obfuscateCode = (sourceCode) => {
   const reFunctionName = /^(?=function).*\(/gm;
   const reOneLineComment = /\/\/[^\r\n]+/g;
   const reMultiLineComment = /\/\*.*?\*\//gs;
+  const reDigit = /^-?\d+\.?\d*$/g;
+  const reWords = /([a-z0-9{}]+)/gi;
   let resultCode = sourceCode.replace(
     reOneLineComment,
     "//" + randomString(145)
@@ -22,7 +24,28 @@ const obfuscateCode = (sourceCode) => {
     reMultiLineComment,
     "/*" + randomString(777) + "*/"
   );
+  const sourceCodeWords = sourceCode.match(reWords);
+  const dictionary = new Map();
+  for (let i in sourceCodeWords) {
+    let countCurlyBrackets = 0;
+    
+    const word = sourceCodeWords[i];
+    
+    if (dictionary.get();
 
+    if (word == "function") {
+      dictionary.set(sourceCodeWords[++i], randomString(18));
+    }
+
+    if (word = '{') countCurlyBrackets++
+    if(word = '}') countCurlyBrackets--
+    let j = 0;
+    
+
+    if (j === dictionary.length) {
+      dictionary.push({ word: word, count: 1 });
+    }
+  }
   //  = sourceCode.replace(reSpaceTab, "");
 
   return resultCode;
